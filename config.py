f0cbfa68406430f513652bdec164708af2e6fe0d
Niels Vandekeybus <progster@gmail.com>
2023-05-22 16:31:11 +0200

    fix default cron pattern

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 CONSUMER_IMAGE = os.getenv("CONSUMER_IMAGE") or "redpencil/ldes-consumer:feature-better-processing"
 MU_NETWORK = os.getenv("MU_NETWORK")
 CONTAINER_LABEL="io.redpencil.ldes-consumer-manager"
-CRON_PATTERN=os.getenv("CRON_PATTERN", "* 0 * * * *")
+CRON_PATTERN=os.getenv("CRON_PATTERN", "0 * * * * *")
 DEFAULT_DEREFERENCE_MEMBERS = bool(os.getenv("DEFAULT_DEREFERENCE_MEMBERS", True))
 DEFAULT_REQUESTS_PER_MINUTE = int(os.getenv("DEFAULT_REQUESTS_PER_MINUTE", 150))
 DEFAULT_REPLACE_VERSIONS = bool(os.getenv("DEFAULT_REPLACE_VERSIONS", True))
