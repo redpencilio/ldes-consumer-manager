f686f38e72fcaa3598196d342bc67ec1ba22653d
Michaël Dierick <michael.dierick@redpencil.io>
2024-07-29 12:20:21 +0200

    add env var config for compose project label

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 CONSUMER_IMAGE = os.getenv("CONSUMER_IMAGE") or "redpencil/ldes-consumer:feature-better-processing"
 MU_NETWORK = os.getenv("MU_NETWORK")
 CONTAINER_LABEL="io.redpencil.ldes-consumer-manager"
+COMPOSE_PROJECT = os.getenv("COMPOSE_PROJECT", None)
 CRON_PATTERN=os.getenv("CRON_PATTERN", "0 * * * * *")
 DEFAULT_DEREFERENCE_MEMBERS = bool(os.getenv("DEFAULT_DEREFERENCE_MEMBERS", True))
 DEFAULT_REQUESTS_PER_MINUTE = int(os.getenv("DEFAULT_REQUESTS_PER_MINUTE", 150))
